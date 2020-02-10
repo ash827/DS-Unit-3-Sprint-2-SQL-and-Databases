8e56fb026b1605a05948522102ebb9d8df004202
Ashley Eddings <ashley.eddings89@gmail.com>
2020-02-09 22:40:19 -0500

    Part 3 Complete

--- northwind.py
+++ northwind.py
@@ -21,3 +21,29 @@ FROM (
 
 #So excited because everything worked when I tried it out 
 #In TablePlus!!!! 
+
+#Part 3 - didn't EXACTLY get the stretch on this one,
+#but I got close!
+
+#/*What are the 10 most expensive items and their suppliers?*/
+SELECT ProductName, UnitPrice, CompanyName
+FROM Product
+JOIN Supplier
+	ON Product.SupplierId = Supplier.Id
+ORDER BY UnitPrice DESC
+LIMIT 10;
+
+#/*What is the largest category(by # of unique prods in it)?*/
+SELECT CategoryName, COUNT(DISTINCT ProductName) as prod_count
+FROM Category
+JOIN Product
+	ON Product.CategoryId = Category.Id
+GROUP BY CategoryName
+ORDER BY prod_count DESC;
+
+#/*Who is the employee with the most territory?*/
+SELECT TerritoryId, EmployeeId
+FROM Employee
+JOIN EmployeeTerritory
+	ON Employee.Id = EmployeeTerritory.EmployeeId
+ORDER BY EmployeeId;
